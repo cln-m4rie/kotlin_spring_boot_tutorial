a4699a31e6546d6c6e6e25145c7e2fdfd85fdd96
cln-m4rie <s.kikkawa0613@gmail.com>
2020-02-01 01:04:31 +0900

    add swagger

--- src/main/kotlin/com/example/kotlin_spring_boot_tutorial/KotlinSpringBootTutorialApplication.kt
+++ src/main/kotlin/com/example/kotlin_spring_boot_tutorial/KotlinSpringBootTutorialApplication.kt
@@ -2,8 +2,10 @@ package com.example.kotlin_spring_boot_tutorial
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
+@EnableSwagger2
 class KotlinSpringBootTutorialApplication
 
 fun main(args: Array<String>) {
